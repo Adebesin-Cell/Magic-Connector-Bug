76c3076ba0b4a03f5dfa38c93ea478814aea2400
Adebesin Cell <adebesintolulope80@gmail.com>
2023-08-23 14:41:42 +0100

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,42 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import { MagicAuthConnector } from '@magiclabs/wagmi-connector';
+import type { AppProps } from 'next/app';
+import { WagmiConfig, configureChains, createConfig, mainnet } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet],
+  [publicProvider()]
+);
+
+const config = createConfig({
+  autoConnect: true,
+  publicClient,
+  webSocketPublicClient,
+  connectors: [
+    new MagicAuthConnector({
+      chains,
+      options: {
+        apiKey: process.env.NEXT_PUBLIC_MAGIC_API_KEY as string,
+        isDarkMode: true,
+        oauthOptions: {
+          providers: ['google', 'facebook', 'twitter', 'discord']
+        },
+        magicSdkConfiguration: {
+          network: {
+            rpcUrl: 'https://rpc.ankr.com/eth',
+            chainId: 1
+          }
+        }
+      }
+    })
+  ]
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig config={config}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  );
 }
